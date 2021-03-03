99cf2b6d4e511a08b2366cbb01ca26da321bf0b8
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-03 06:38:03 +0000

    importando pacote provider e alterando a classe slider

--- lib/controllers/carousel_controller.dart
+++ lib/controllers/carousel_controller.dart
@@ -3,10 +3,16 @@ import 'package:flutter/foundation.dart';
 import 'package:hinario_catolico/models/carousel_model.dart';
 
 class CarouselController extends ChangeNotifier {
-  CarouselController();
+  CarouselController() {
+    getCarousel();
+  }
 
   List<CarouselModel> lista = [];
   final Firestore firestore = Firestore.instance;
+
+  int countSlider() {
+    return lista.length;
+  }
 
   Future<void> getCarousel() async {
     final getLista = await firestore.collection("carousel").getDocuments();
