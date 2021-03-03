fe39339960101152a3a6d54853ae7ad1a3f7fc3d
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-03 05:58:48 +0000

    alterando a fonte das imagens do carousel

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:hinario_catolico/widgets/drawer_screen.dart';
 
@@ -5,31 +6,103 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Hinário Católico'),
+        title: Text('Hinário'),
       ),
       drawer: DrawerScreen(),
-      body: Column(children: [
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: Text("Frase do dia"),
-        ),
-        Container(
-          child: Text("Galeria"),
-        ),
-        Row(
-          children: [
-            Container(
-              child: Text("hello1"),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 150.0,
+            width: double.infinity,
+            child: Carousel(
+              dotSize: 4.0,
+              dotSpacing: 15.0,
+              dotColor: Colors.lightGreenAccent,
+              indicatorBgPadding: 15.0,
+              dotBgColor: Colors.black38,
+              dotVerticalPadding: 0.0,
+              dotPosition: DotPosition.bottomRight,
+              images: [
+                InkWell(
+                  onTap: () {
+                    print("clicou 1");
+                  },
+                  child: Image.network(
+                      'https://img2.migalhas.uol.com.br/_MEDPROC_/https__img1.migalhas.uol.com.br__SL__gf_base__SL__empresas__SL__MIGA__SL__imagens__SL__2020__SL__10__SL__27__SL__4b0aea2f-b96d-44d1-91a3-f0596ef846f8.jpg._PROC_CP75.jpg',
+                      fit: BoxFit.cover),
+                ),
+                InkWell(
+                  onTap: () {
+                    print("clicou 2");
+                  },
+                  child: Image.network(
+                    'http://gazetadotriangulo.com.br/wp-content/uploads/2019/04/Semana-Santa-606x312.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    print("clicou 3");
+                  },
+                  child: Image.network(
+                    'https://www.je10.com.br/wp-content/uploads/2019/04/Comunidades-cat%C3%B3licas-divulgam-celebra%C3%A7%C3%B5es-da-semana-santa-777x437.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
             ),
-            Container(
-              child: Text("hello2"),
-            )
-          ],
-        ),
-      ]),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "Boas Novas",
+            style: TextStyle(
+              color: Colors.amber,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+         SingleChildScrollView(
+           child: ConstrainedBox(
+             constraints: BoxConstraints(
+             ),
+             child: Column(
+               mainAxisSize: MainAxisSize.min,
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
+               children: <Widget>[
+                 Container(
+                   // A fixed-height child.
+                   color: const Color(0xffeeee00), // Yellow
+                   height: 120.0,
+                   alignment: Alignment.center,
+                   child: const Text('Fixed Height Content'),
+                 ),
+                 Container(
+                   // Another fixed-height child.
+                   color: const Color(0xff008000), // Green
+                   height: 120.0,
+                   alignment: Alignment.center,
+                   child: const Text('Fixed Height Content'),
+                 ),
+               ],
+             ),
+           ),
+         ),
+        ],
+      ),
     );
   }
+
+lista(){
+  return Container(
+    
+  );
+}
+   
 }
