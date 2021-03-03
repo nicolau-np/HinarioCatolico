01e0c4fcf97ef038188b599096b4ea88aa8dcb8c
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-03 06:45:40 +0000

    creando widget Carousel

---
+++ lib/widgets/carousel_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class CarouselWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
