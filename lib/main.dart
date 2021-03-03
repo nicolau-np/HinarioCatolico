2e6dd21f2af054874765250d937b7d199fbea449
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-03 06:41:11 +0000

    importando os providers para o nosso arquivo main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hinario_catolico/controllers/carousel_controller.dart';
+import 'package:hinario_catolico/models/carousel_model.dart';
 import 'package:hinario_catolico/screens/canticos.dart';
 import 'package:hinario_catolico/screens/configuracoes.dart';
 import 'package:hinario_catolico/screens/home_page.dart';
 import 'package:hinario_catolico/screens/oracoes.dart';
 import 'package:hinario_catolico/screens/suporte.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,22 +16,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hinário Católico',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CarouselController()),
+        ChangeNotifierProvider(create: (_) => CarouselModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Hinário Católico',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          primarySwatch: Colors.red,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/home',
+        routes: {
+          '/home': (context) => HomePage(),
+          '/oracoes': (context) => OracoesPage(),
+          '/canticos': (context) => CanticosPage(),
+          '/configuracoes': (context) => ConfigPage(),
+          '/suporte': (context) => SuportePage(),
+        },
       ),
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => HomePage(),
-        '/oracoes': (context) => OracoesPage(),
-        '/canticos': (context) => CanticosPage(),
-        '/configuracoes': (context) => ConfigPage(),
-        '/suporte': (context) => SuportePage(),
-      },
     );
   }
 }
