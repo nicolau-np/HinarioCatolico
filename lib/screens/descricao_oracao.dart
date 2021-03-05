3be1c139e374158714be051ac3e63438ab11ac23
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 22:24:11 +0000

    terminando a descricao de oracoes

--- lib/screens/descricao_oracao.dart
+++ lib/screens/descricao_oracao.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:hinario_catolico/models/oracao_model.dart';
 
 class DescricaoOracao extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final route = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final oracao = ModalRoute.of(context).settings.arguments as OracaoModel;
     return Scaffold(
       appBar: AppBar(
-        title: Text(route['title']),
+        title: Text(oracao.title),
       ),
-      body: Center(
-        child: Text('hello'),
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.share,
+                    color: Colors.red,
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.copy,
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 7,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(oracao.bDescription),
+            ),
+          ),
+        ],
       ),
     );
   }
