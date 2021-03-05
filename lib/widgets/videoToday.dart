784b2425e17507e29941dca7e49b4efb4ed2fe4b
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 13:20:36 +0000

    adicionando video

--- lib/widgets/videoToday.dart
+++ lib/widgets/videoToday.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
 class VideoToday extends StatefulWidget {
   @override
   _VideoTodayState createState() => _VideoTodayState();
 }
 
 class _VideoTodayState extends State<VideoToday> {
+  VideoPlayerController playerController;
+  VoidCallback listener;
+
   @override
-   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.amber,
-        boxShadow: [
+  void initState() {
+    super.initState();
+    listener = () {
+      setState(() {});
+    };
+  }
+
+  void createVideo() {
+    if (playerController == null) {
+      playerController = VideoPlayerController.network(
+          "https://www.youtube.com/watch?v=7IG5kRFIMZw")
+        ..addListener(listener)
+        ..setVolume(1.0)
+        ..initialize();
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){
+        createVideo();
+        playerController.play();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.amber,
+          boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.5),
               spreadRadius: 1,
@@ -19,9 +48,19 @@ class _VideoTodayState extends State<VideoToday> {
               offset: Offset(0, 2), // changes position of shadow
             ),
           ],
+        ),
+        height: 170,
+        width: 300,
+        child: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: Container(
+            child: (playerController!=null?VideoPlayer(playerController): Container()),
+          ),
+        ),
+
+      
       ),
-      height: 170,
-      width: 300,
     );
+    
   }
 }
