715e32ccfde8b21f8f1205dcf84c19d2f98fb868
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 19:02:39 +0000

    update oracoes

--- lib/screens/oracoes.dart
+++ lib/screens/oracoes.dart
@@ -44,62 +44,86 @@ class OracoesPage extends StatelessWidget {
   }
 
   _container() {
-    return Container(
-      height: 100,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(6),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 1,
-            blurRadius: 7,
-            offset: Offset(0, 2), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: double.infinity,
-              width: 100,
-              child: Image.network(
-                'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80',
-                fit: BoxFit.cover,
-              ),
+    return InkWell(
+      onTap: (){
+        print('hello');
+      },
+          child: Container(
+        height: 100,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 7,
+              offset: Offset(0, 2), // changes position of shadow
             ),
-            SizedBox(
-              width: 10,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Titulo',
-                  style: TextStyle(
-                    color: Colors.red[200],
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                Text(
-                  'Descricao da oracao lorem ipsium lorem',
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 12,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                
-              ],
-            )
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: double.infinity,
+                width: 100,
+                child: Image.network(
+                  'https://image.flaticon.com/icons/png/512/42/42943.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Titulo',
+                    style: TextStyle(
+                      color: Colors.red[200],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    'Descricao da oracao lorem ipsium lorem',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 12,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.share,
+                          color: Colors.red,
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.copy,
+                          color: Colors.red,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
