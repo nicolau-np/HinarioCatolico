d89e80a984927c8c09e6927aa0adcf8c579d3d04
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 19:22:43 +0000

    terminada tela de descriaco oracao

---
+++ lib/screens/descricao_oracao.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DescricaoOracao extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final route = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(route['title']),
+      ),
+      body: Center(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
