74d49407d7c8013234042603d65f4a2537d3fbe1
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 11:34:46 +0000

    mudando a tela home

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:hinario_catolico/widgets/cards.dart';
 import 'package:hinario_catolico/widgets/carousel_widget.dart';
-import 'package:hinario_catolico/widgets/circleButton.dart';
+import 'package:hinario_catolico/widgets/circle_button.dart';
 import 'package:hinario_catolico/widgets/drawer_screen.dart';
 
 class HomePage extends StatelessWidget {
@@ -25,41 +25,29 @@ class HomePage extends StatelessWidget {
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: [
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: CardsWidget(
-                      "https://www.vaticannews.va/content/dam/vaticannews/multimedia/2020/02/07/00891_07022020-Sua-Beatitudine-Em.ma-Card.-Louis-RaphaEl-Sako-Patriarca-di-Babilonia-dei-C.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.750.422.jpeg",
-                      'Visita à Angola',
-                      'Papa Francisco faz a sua visita a Angola',
-                      "05-03-2021    12:15:00",
-                    ),
+                  CardsWidget(
+                    "https://www.vaticannews.va/content/dam/vaticannews/multimedia/2020/02/07/00891_07022020-Sua-Beatitudine-Em.ma-Card.-Louis-RaphaEl-Sako-Patriarca-di-Babilonia-dei-C.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.750.422.jpeg",
+                    'Visita à Angola',
+                    'Papa Francisco faz a sua visita a Angola',
+                    "05-03-2021    12:15:00",
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: CardsWidget(
-                      "https://www12.senado.leg.br/noticias/materias/2019/02/12/humberto-costa-critica-interferencia-da-abin-na-igreja-catolica/20190212_00249s.jpg",
-                      'Igrejas Catolicas fechadas',
-                      'Governo de Paquistão fecha as igrejas catolicas',
-                      "02-03-2021    10:10:00",
-                    ),
+                  CardsWidget(
+                    "https://www12.senado.leg.br/noticias/materias/2019/02/12/humberto-costa-critica-interferencia-da-abin-na-igreja-catolica/20190212_00249s.jpg",
+                    'Igrejas Catolicas fechadas',
+                    'Governo de Paquistão fecha as igrejas catolicas',
+                    "02-03-2021    10:10:00",
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: CardsWidget(
-                      "https://cdn.domtotal.com/img/noticias/2020-12/1488017_470927.jpg",
-                      'Vaticano de volta',
-                      'Vaticano abre as portas após a Pandemia',
-                      "10-01-2021    17:50:00",
-                    ),
+                  CardsWidget(
+                    "https://cdn.domtotal.com/img/noticias/2020-12/1488017_470927.jpg",
+                    'Vaticano de volta',
+                    'Vaticano abre as portas após a Pandemia',
+                    "10-01-2021    17:50:00",
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: CardsWidget(
-                      "https://www.sonoticias.com.br/wp-content/uploads/2018/01/5tl9nt7eormccb2iw9mrkdsznfvejtos8nulzy4jskcvvkz9.jpg",
-                      'Secretário do Papa',
-                      'Secretário do Papa fala para a Rádio',
-                      "25-12-2020    20:03:1",
-                    ),
+                  CardsWidget(
+                    "https://www.sonoticias.com.br/wp-content/uploads/2018/01/5tl9nt7eormccb2iw9mrkdsznfvejtos8nulzy4jskcvvkz9.jpg",
+                    'Secretário do Papa',
+                    'Secretário do Papa fala para a Rádio',
+                    "25-12-2020    20:03:1",
                   ),
                 ],
               ),
@@ -67,10 +55,8 @@ class HomePage extends StatelessWidget {
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleButton(),
-                ),
+                CircleButton(),
+                
               ],
             ),
           ],
--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -9,67 +9,70 @@ class CardsWidget extends StatelessWidget {
   CardsWidget(this.image, this.title, this.description, this.date);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 1,
-            blurRadius: 7,
-            offset: Offset(0, 2), // changes position of shadow
-          ),
-        ],
-      ),
-      width: 200,
-      height: 550,
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image(
-              image: NetworkImage(this.image),
-              height: 110.0,
-              width: double.infinity,
-              fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 7,
+              offset: Offset(0, 2), // changes position of shadow
             ),
-            Padding(
-              padding: EdgeInsets.all(4),
-              child: Text(
-                this.title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                ),
+          ],
+        ),
+        width: 200,
+        height: 550,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image(
+                image: NetworkImage(this.image),
+                height: 110.0,
+                width: double.infinity,
+                fit: BoxFit.cover,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(4),
-              child: Text(
-                this.description,
-                style: TextStyle(color: Colors.black38, fontSize: 12),
-              ),
-            ),
-            Padding(
+              Padding(
                 padding: EdgeInsets.all(4),
                 child: Text(
-                  this.date,
+                  this.title,
                   style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 8,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
                   ),
-                )),
-            Center(
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text("Ler mais"),
-                color: Colors.red,
-                textColor: Colors.white,
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.all(4),
+                child: Text(
+                  this.description,
+                  style: TextStyle(color: Colors.black38, fontSize: 12),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(4),
+                  child: Text(
+                    this.date,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 8,
+                    ),
+                  )),
+              Center(
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Ler mais"),
+                  color: Colors.red,
+                  textColor: Colors.white,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
