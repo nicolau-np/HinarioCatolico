83689d4eabfde7c0a0c8421c9ef46fc59e71b392
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 19:38:36 +0000

    alterando oracoes screen

--- lib/screens/oracoes.dart
+++ lib/screens/oracoes.dart
@@ -34,8 +34,34 @@ class OracoesPage extends StatelessWidget {
                   padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
                   child: ListView(
                     children: [
-                      CardsOracoes('Pai Nosso','Pai nosso que estais no ceus, santif...',),
-                      
+                      CardsOracoes(
+                        'Pai Nosso',
+                        'Pai nosso que estais no ceus, santif...',
+                      ),
+                      CardsOracoes(
+                        'Glória ao Pai',
+                        'Glória ao Pai e ao Filho',
+                      ),
+                      CardsOracoes(
+                        'À Vossa Proteção',
+                        'À Vossa Proteção recorremos, Santa...',
+                      ),
+                      CardsOracoes(
+                        'Alma de Cristo',
+                        'Alma de Cristo, santificai-me...',
+                      ),
+                      CardsOracoes(
+                        'Ao Anjo da Guarda',
+                        'Santo Anjo do Senhor...',
+                      ),
+                      CardsOracoes(
+                        'Benedictus',
+                        'Bendito o Senhor Deus de Israel...',
+                      ),
+                      CardsOracoes(
+                        'Magnificat',
+                        'A minha alma glorifica ao Senhor...',
+                      ),
                     ],
                   ),
                 ),
@@ -46,6 +72,4 @@ class OracoesPage extends StatelessWidget {
       ),
     );
   }
-
-
 }
