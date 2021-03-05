53ac69b69b9f42eb290e3fe644be40b1c9455620
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 21:40:53 +0000

    terminado screen descricao

--- lib/screens/descricao_noticia.dart
+++ lib/screens/descricao_noticia.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:hinario_catolico/models/noticia_model.dart';
 
 class DescricaoNoticia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final route =
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final noticia = ModalRoute.of(context).settings.arguments as NoticiaModel;
     return Scaffold(
       appBar: AppBar(
-        title: Text(route['title']),
+        title: Text(noticia.title),
       ),
-      body: Center(
-        child: Text('ºhello'),
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            width: double.infinity,
+            child: Image.network(
+              noticia.image,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 7,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(noticia.bDescription),
+            ),
+          ),
+        ],
       ),
     );
   }
