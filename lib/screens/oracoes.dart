fa49139ddeeeb0e5ba7e07fb145cab658d8e93dc
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 18:48:22 +0000

    update oracoes

--- lib/screens/oracoes.dart
+++ lib/screens/oracoes.dart
@@ -30,7 +30,7 @@ class OracoesPage extends StatelessWidget {
                 height: 525.0,
                 width: double.infinity,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 4, 30, 4),
+                  padding: const EdgeInsets.fromLTRB(5, 4, 5, 4),
                   child: ListView(
                     children: [_container()],
                   ),
@@ -45,11 +45,63 @@ class OracoesPage extends StatelessWidget {
 
   _container() {
     return Container(
+      height: 100,
+      width: double.infinity,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(6),
-        color: Colors.black26,
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 7,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+        ],
       ),
-      child: Text('hello'),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: double.infinity,
+              width: 100,
+              child: Image.network(
+                'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80',
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Titulo',
+                  style: TextStyle(
+                    color: Colors.red[200],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  'Descricao da oracao lorem ipsium lorem',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 12,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
