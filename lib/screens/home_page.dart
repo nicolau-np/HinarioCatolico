316427565a49ad464054045fe0c62a1196a91144
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 21:25:39 +0000

    alteracao feita em cards noticia pegando os dados da base de dados

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -34,35 +34,7 @@ class HomePage extends StatelessWidget {
               height: 250.0,
               width: double.infinity,
               color: Colors.white,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  CardsWidget(
-                    "https://www.vaticannews.va/content/dam/vaticannews/multimedia/2020/02/07/00891_07022020-Sua-Beatitudine-Em.ma-Card.-Louis-RaphaEl-Sako-Patriarca-di-Babilonia-dei-C.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.750.422.jpeg",
-                    'Visita à Angola',
-                    'Papa Francisco faz a sua visita a Angola',
-                    "05-03-2021    12:15:00",
-                  ),
-                  CardsWidget(
-                    "https://www12.senado.leg.br/noticias/materias/2019/02/12/humberto-costa-critica-interferencia-da-abin-na-igreja-catolica/20190212_00249s.jpg",
-                    'Igrejas Catolicas fechadas',
-                    'Governo de Paquistão fecha as igrejas catolicas',
-                    "02-03-2021    10:10:00",
-                  ),
-                  CardsWidget(
-                    "https://cdn.domtotal.com/img/noticias/2020-12/1488017_470927.jpg",
-                    'Vaticano de volta',
-                    'Vaticano abre as portas após a Pandemia',
-                    "10-01-2021    17:50:00",
-                  ),
-                  CardsWidget(
-                    "https://www.sonoticias.com.br/wp-content/uploads/2018/01/5tl9nt7eormccb2iw9mrkdsznfvejtos8nulzy4jskcvvkz9.jpg",
-                    'Secretário do Papa',
-                    'Secretário do Papa fala para a Rádio',
-                    "25-12-2020    20:03:1",
-                  ),
-                ],
-              ),
+              child: CardsWidget(),
             ),
             SizedBox(
               height: 10,
--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -1,84 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:hinario_catolico/controllers/noticia_controller.dart';
+import 'package:provider/provider.dart';
 
 class CardsWidget extends StatelessWidget {
-  final String image;
-  final String title;
-  final String description;
-  final String date;
-
-  CardsWidget(this.image, this.title, this.description, this.date);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 1,
-              blurRadius: 7,
-              offset: Offset(0, 2), // changes position of shadow
+    return Consumer<NoticiaController>(builder: (__, noticiaController, _) {
+      return ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: noticiaController.countNoticia(),
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 7,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ],
             ),
-          ],
-        ),
-        width: 200,
-        height: 550,
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image(
-                image: NetworkImage(this.image),
-                height: 110.0,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-              Padding(
-                padding: EdgeInsets.all(4),
-                child: Text(
-                  this.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
+            width: 220,
+            height: 550,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                    image: NetworkImage(noticiaController.lista[index].image),
+                    height: 110.0,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(4),
-                child: Text(
-                  this.description,
-                  style: TextStyle(color: Colors.black38, fontSize: 12),
-                ),
-              ),
-              Padding(
-                  padding: EdgeInsets.all(4),
-                  child: Text(
-                    this.date,
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 8,
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: Text(
+                      noticiaController.lista[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
                     ),
-                  )),
-              Center(
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/descricao_noticia', arguments: {
-                      'title':this.title
-                    });
-                  },
-                  child: Text("Ler mais"),
-                  color: Colors.red,
-                  textColor: Colors.white,
-                ),
-              )
-            ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: Text(
+                      noticiaController.lista[index].pDescription,
+                      style: TextStyle(color: Colors.black38, fontSize: 12),
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.all(4),
+                      child: Text(
+                        noticiaController.lista[index].data,
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 8,
+                        ),
+                      )),
+                  Center(
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/descricao_noticia',
+                            arguments: {'title': 'titulo'});
+                      },
+                      child: Text("Ler mais"),
+                      color: Colors.red,
+                      textColor: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        );
+  
+        },
+      );
+    });
   }
 }
