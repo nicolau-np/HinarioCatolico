bddf7f65d3ae846ca4501adad52a0fee75945e46
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 22:29:56 +0000

    dando ultimos detalhes em descricao da oracao

--- lib/screens/descricao_oracao.dart
+++ lib/screens/descricao_oracao.dart
@@ -11,51 +11,63 @@ class DescricaoOracao extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            height: 100,
-            width: double.infinity,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Icon(
-                    Icons.share,
-                    color: Colors.red,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+            child: Container(
+              height: 20,
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.share,
+                      color: Colors.red,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Icon(
-                    Icons.copy,
-                    color: Colors.red,
+                  SizedBox(
+                    width: 20,
                   ),
-                )
-              ],
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.copy,
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           SizedBox(
             height: 10,
           ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 7,
-                  offset: Offset(0, 2), // changes position of shadow
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: Offset(0, 2), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  oracao.bDescription,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.justify,
                 ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(oracao.bDescription),
+              ),
             ),
           ),
         ],
