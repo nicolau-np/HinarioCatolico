aa68d3b0eb784c33dbf1780ba7585452aef06fe2
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 10:04:00 +0000

    trabalhando no cards

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:hinario_catolico/widgets/cards.dart';
 import 'package:hinario_catolico/widgets/carousel_widget.dart';
 import 'package:hinario_catolico/widgets/drawer_screen.dart';
 
@@ -18,13 +19,16 @@ class HomePage extends StatelessWidget {
           children: [
             CarouselWidget(),
             Container(
-              height: 100,
+              height: 200.0,
               width: double.infinity,
               color: Colors.red,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: [
-                  
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                    child: CardsWidget(),
+                  ),
                 ],
               ),
             ),
@@ -32,9 +36,5 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  _card() {
-    
   }
 }
--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -3,67 +3,23 @@ import 'package:flutter/material.dart';
 class CardsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            "https://i.pinimg.com/originals/ca/76/0b/ca760b70976b52578da88e06973af542.jpg",
-                          ),
-                          backgroundColor: Colors.transparent,
-                        ),
-                        title: Text(
-                          "Novas escolas",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            color: Colors.blue,
-                          ),
-                        ),
-                        subtitle: Text("hello"),
-                      ),
-                      Container(
-                        alignment: Alignment.center,
-                        margin: EdgeInsets.all(0),
-                        child: Image.network(
-                          "http://webmeup.com/upload/blog/lead-image-105.png",
-                          fit: BoxFit.fill,
-                          height: 120,
-                          width: 350,
-                        ),
-                      ),
-                      Container(
-                        // make buttons use the appropriate styles for cards
-                        child: ButtonBar(
-                          children: <Widget>[
-                            Text(
-                              "nada",
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            FlatButton(
-                              child: const Text('Explorar'),
-                              onPressed: () {
-                                
-                              },
-                              textColor: Colors.white,
-                              color: Colors.amber,
-                            ),
-                            FlatButton(
-                              child: const Text('Reservar'),
-                              onPressed: () {/* ... */},
-                              textColor: Colors.white,
-                              color: Colors.amber,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      width: 200,
+      height: double.infinity,
+      child: Padding(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: NetworkImage('https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg'), height: 50,),
+            Text('hello2')
+          ],
+        ),
+      ),
+    );
   }
 }
