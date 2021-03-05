0f960b319ea2b10c249d3f15fb2588aeb9764ec7
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-05 12:52:27 +0000

    trabalhando na tela de video

--- lib/widgets/videoToday.dart
+++ lib/widgets/videoToday.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+
 class VideoToday extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.amber,
+        boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 7,
+              offset: Offset(0, 2), // changes position of shadow
+            ),
+          ],
+      ),
+      height: 170,
+      width: 300,
     );
   }
 }
