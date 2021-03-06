8c9bd68c97682da9f279616980d0a1cca0140d33
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-06 08:32:04 +0000

    terminado pegando imagens do carousel apartir da BD

--- lib/widgets/carousel_widget.dart
+++ lib/widgets/carousel_widget.dart
@@ -1,47 +1,39 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:hinario_catolico/controllers/carousel_controller.dart';
+import 'package:provider/provider.dart';
 
 class CarouselWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 150.0,
-      width: double.infinity,
-      child: Carousel(
-        dotSize: 4.0,
-        dotSpacing: 15.0,
-        dotColor: Colors.lightGreenAccent,
-        indicatorBgPadding: 15.0,
-        dotBgColor: Colors.black38,
-        dotVerticalPadding: 0.0,
-        dotPosition: DotPosition.bottomRight,
-        images: [
-          InkWell(
-            onTap: () {
-              print("clicou 1");
-            },
-            child: Image.network(
-                'https://img2.migalhas.uol.com.br/_MEDPROC_/https__img1.migalhas.uol.com.br__SL__gf_base__SL__empresas__SL__MIGA__SL__imagens__SL__2020__SL__10__SL__27__SL__4b0aea2f-b96d-44d1-91a3-f0596ef846f8.jpg._PROC_CP75.jpg',
-                fit: BoxFit.cover),
-          ),
-          InkWell(
-            onTap: () {
-              print("clicou 1");
-            },
-            child: Image.network(
-                'https://www.iol.pt/multimedia/oratvi/multimedia/imagem/id/5c6e567b0cf232f8cfbbefca/1024',
-                fit: BoxFit.cover),
-          ),
-          InkWell(
-            onTap: () {
-              print("clicou 1");
-            },
-            child: Image.network(
-                'https://miro.medium.com/max/1200/1*EiY8N0kZQXTbW8hn6WqjBw.jpeg',
-                fit: BoxFit.cover),
-          ),
-        ],
-      ),
-    );
+    return Consumer<CarouselController>(builder: (__, carouselController,_){
+      return  SizedBox(
+        height: 170.0,
+        width: double.infinity,
+        child: Carousel(
+          dotSize: 4.0,
+          dotSpacing: 15.0,
+          dotColor: Colors.lightGreenAccent,
+          indicatorBgPadding: 15.0,
+          dotBgColor: Colors.black38,
+          dotVerticalPadding: 0.0,
+          dotPosition: DotPosition.bottomRight,
+          images: carouselController.lista.map((e) {
+            return Builder(builder: (BuildContext context){
+              return InkWell(
+                onTap: () {
+                  print("clicou 1");
+                },
+                child: Image.network(
+                    e.imagem,
+                    fit: BoxFit.fill,),
+              );
+            });
+          }).toList(),
+        ),
+      );
+    });
   }
+
+
 }
