317efbd386879dbcf5b133d17dfdb0fcf7b24d76
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-06 10:16:53 +0000

    terminado os modulos

--- lib/controllers/cantico_controller.dart
+++ lib/controllers/cantico_controller.dart
@@ -1,15 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hinario_catolico/models/cantico_model.dart';
 
-class CanticoController extends ChangeNotifier{
-List<CanticoModel> lista = [];
-CanticoController(){
+class CanticoController extends ChangeNotifier {
+  Firestore firestore = Firestore.instance;
+  List<CanticoModel> lista = [];
+  CanticoController() {
+    getCanticos();
+  }
 
-}
+  int countCanticos() {
+    return lista.length;
+  }
 
-int countCanticos(){
-  return lista.length;
-}
+  Future<CanticoModel> getCanticos() async {
+    final lista_canticos =
+        await firestore.collection('canticos').getDocuments();
+    lista = lista_canticos.documents
+        .map((e) => (CanticoModel.fromDocument(e)))
+        .toList();
 
-Future<CanticoModel>
+    notifyListeners();
+  }
 }
